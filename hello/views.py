10ddf8806b6a2b1e372eb248e35ec0090668c187
Danny <ddeneau@terpmail.umd.edu>
2023-04-09 18:11:45 -0400

    dasd

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,11 @@
-from flask import jsonify
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Greeting
 
 # Create your views here.
-def index():
+def index(request):
     # return HttpResponse('Hello from Python!')
-    return jsonify({'hello': 'world'})
+    return render(HttpResponse,'32')
+
+
